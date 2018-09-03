4894c1267a3e8dd5fa5fcc1d74fcd892223cf8fe
Tom Brereton <Tom.Brereton@asos.com>
2018-09-03 17:48:58 +0100

    Moved FizzBuzzCalcalator to diff project

---
+++ FizzBuzzCalcaulatorTests/FizzBuzzCalculatorShould.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace FizzBuzzCalcaulatorTests
+{
+    public class FizzBuzzCalculatorShould
+    {
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, "Fizz")]
+        [TestCase(6, "Fizz")]
+        [TestCase(9, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(10, "Buzz")]
+        [TestCase(20, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(30, "FizzBuzz")]
+        [TestCase(45, "FizzBuzz")]
+        [TestCase(89, "89")]
+        [TestCase(75, "FizzBuzz")]
+        [TestCase(99, "Fizz")]
+        public void CalcultateIfNumberShouldBeFizzOrBuzzOrFizzBuzz(int number, string expectedFizzBuzz)
+        {
+            var actualFizzBuzz = FizzBuzzCalculator.FizzBuzzCalculator.Convert(number);
+
+            Assert.That(actualFizzBuzz, Is.EqualTo(expectedFizzBuzz));
+        }
+    }
+}
---
+++ FizzBuzzCalculator/FizzBuzzCalculator.cs
@@ -0,0 +1,22 @@
+﻿namespace FizzBuzzCalculator
+{
+    public class FizzBuzzCalculator
+    {
+        public static string Convert(int number)
+        {
+            if (number % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            return number.ToString();
+        }
+    }
+}
